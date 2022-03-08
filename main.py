fa719c8a006c39a869a96fb6bfc75499e8328cc2
Esmat Zahid <zahidm15@uni.coventry.ac.uk>
2022-03-08 23:29:15 +0000

    extract util helper functions and move them into their own file

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 
 from constants import DB_PATH, UPLOAD_FOLDER, ALLOWED_EXTENSIONS
 from login import get_login_details
+from utils import parse
 
 app = Flask(__name__)
 app.secret_key = 'random string'
@@ -391,20 +392,6 @@ def registrationForm():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
-def parse(data):
-    ans = []
-    i = 0
-    while i < len(data):
-        curr = []
-        for j in range(7):
-            if i >= len(data):
-                break
-            curr.append(data[i])
-            i += 1
-        ans.append(curr)
-    return ans
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,12 @@
+def parse(data):
+    ans = []
+    i = 0
+    while i < len(data):
+        curr = []
+        for j in range(7):
+            if i >= len(data):
+                break
+            curr.append(data[i])
+            i += 1
+        ans.append(curr)
+    return ans
